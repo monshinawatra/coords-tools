77058a660ef51c44242c92fe34b5e00c0b72d9e6
monshinawatra <monshinawatra@gmail.com>
2024-07-31 16:01:11 +0700

    Update

--- coords_tools/main.py
+++ coords_tools/main.py
@@ -27,49 +27,59 @@ def get_area_coords(frame: np.ndarray, length:int) -> np.ndarray:
     # Initialize global variables
     is_button_down, corner_points_list = False, []
 
-    # Initialize window
-    windowName = "MouseCallback"
-    cv2.namedWindow(windowName)
-    cv2.setMouseCallback(windowName, CallBackFunc, (corner_points_list))
+    try:
+        # Initialize window
+        windowName = "MouseCallback"
+        cv2.namedWindow(windowName)
+        cv2.setMouseCallback(windowName, CallBackFunc, (corner_points_list))
 
-    # Main loop
-    while cv2.getWindowProperty(windowName, 0) >= 0:
-        im0s = frame.copy()
-        image = cv2.resize(im0s, (960, 540))
-        # Draw points (Real-time)
-        for i, point in enumerate(corner_points_list):
-            cv2.circle(image, point, 7, (255, 255, 255), -1)
-            cv2.circle(image, point, 5, POINT_COLORS[i], -1)
+        # Main loop
+        while cv2.getWindowProperty(windowName, 0) >= 0:
+            im0s = frame.copy()
+            image = cv2.resize(im0s, (960, 540))
+            # Draw points (Real-time)
+            for i, point in enumerate(corner_points_list):
+                cv2.circle(image, point, 7, (255, 255, 255), -1)
+                cv2.circle(image, point, 5, POINT_COLORS[i], -1)
 
-        # Draw lines connecting between points (Real-time)
-        for i in range(len(corner_points_list)):
-            if i == len(corner_points_list) - 1:
-                if (len(corner_points_list) > 2):
-                    cv2.line(
-                        image,
-                        corner_points_list[i],
-                        corner_points_list[0],
-                        (0, 255, 0),
-                        1,
-                    )
-                continue
+            # Draw lines connecting between points (Real-time)
+            for i in range(len(corner_points_list)):
+                if i == len(corner_points_list) - 1:
+                    if (len(corner_points_list) > 2):
+                        cv2.line(
+                            image,
+                            corner_points_list[i],
+                            corner_points_list[0],
+                            (0, 255, 0),
+                            1,
+                        )
+                    continue
 
-            cv2.line(
-                image,
-                corner_points_list[i],
-                corner_points_list[i+1],
-                (0, 255, 0),
-                1,
-            )
+                cv2.line(
+                    image,
+                    corner_points_list[i],
+                    corner_points_list[i+1],
+                    (0, 255, 0),
+                    1,
+                )
 
-        # Show image
-        cv2.imshow(windowName, image)
-        # Exit if already have 4 points
-        if cv2.waitKey(1) and (len(corner_points_list) >= length and not is_button_down):
-            break
-
-    # Destroy all cv2 windows
-    cv2.destroyAllWindows()
+            # Show image
+            cv2.imshow(windowName, image)
+            # Exit if already have 4 points
+            # if cv2.waitKey(1) and (len(corner_points_list) >= length and not is_button_down):
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                corner_points_list = np.array(corner_points_list, dtype=np.float32)
+                # Normalize points
+                corner_points_list /= np.array([960, 540], dtype=np.float32)
+                # Multiply by image size
+                corner_points_list *= np.array([frame.shape[1], frame.shape[0]], dtype=np.float32)
+                break
+    except Exception as e:
+        print(e)
+    
+    finally:
+        # Destroy all cv2 windows
+        cv2.destroyAllWindows()
     return corner_points_list
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coords-tools',
-    version='0.1',
+    version='0.2',
     packages=['coords_tools'],
     install_requires=[
         'numpy',
